5bc1fb636799de1d6a8ca77106756f4bb64547ae
DotNotFact <KilYatti@gmail.com>
2024-01-23 17:44:12 +0300

    Решение задачи #1 "Морской бой"

--- OzoneTraining/OzoneTraining/Program.cs
+++ OzoneTraining/OzoneTraining/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int.TryParse(Console.ReadLine(), out int size); 
+var size = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < size; i++)
+{
+    string[] ships = Console.ReadLine().Split(' ');
+    int[] field = Array.ConvertAll(ships, int.Parse);
+    Console.WriteLine(CheckIsValid(field));
+}
+
+static string CheckIsValid(int[] ships)
+{
+    int[] count = new int[5];
+
+    foreach (int ship in ships)
+        count[ship]++;
+
+    var result = (count[1] == 4) && (count[2] == 3) && (count[3] == 2) && (count[1] == 4) ? "Yes" : "No";
+    return result;
+}
