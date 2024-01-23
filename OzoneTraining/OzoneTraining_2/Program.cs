f928a5b47d3002d41f5d865cec4ccdc0a5850e5c
DotNotFact <KilYatti@gmail.com>
2024-01-23 19:56:14 +0300

    Решение задачи #2 "Проверка даты"

--- OzoneTraining/OzoneTraining_2/Program.cs
+++ OzoneTraining/OzoneTraining_2/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int t = int.Parse(Console.ReadLine()); // 10
+
+// string[] s1 = {
+//    "10 9 2022",
+//    "21 9 2022",
+//    "29 2 2022",
+//    "31 2 2022",
+//    "29 2 2000",
+//    "29 2 2100",
+//    "31 11 1999",
+//    "31 12 1999",
+//    "29 2 2024",
+//    "29 2 2023"
+// };
+
+for (int i = 0; i < t; i++)
+{
+    string[] temp = Console.ReadLine().Split(' ');
+    int[] date = temp.Select(int.Parse).ToArray();
+
+    bool isDateValid = IsValidDate(date[0], date[1], date[2]);
+
+    Console.WriteLine(isDateValid ? "Yes" : "No");
+}
+
+static bool IsValidLeapYear(int y)
+{
+    return ((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0);
+}
+
+static bool IsValidDate(int d, int m, int y)
+{
+    if (((d < 1) && (d > 31)) && ((m < 1) && (m > 12)) && ((y < 1950) && (y > 2300)))
+        return false;
+
+    if (m == 2)
+        return d <= (IsValidLeapYear(y) ? 29 : 28);
+
+    if ((m == 4) || (m == 6) || (m == 9) || (m == 11))
+        return d <= 30;
+
+    return true;
+}
