83bfd3d75dc9975d35837baefc18a7cff3b45d52
DotNotFact <KilYatti@gmail.com>
2024-01-25 20:42:10 +0300

    Решение задачи #4 "Битва за кондиционер"

--- OzoneTraining/OzoneTraining_4/Program.cs
+++ OzoneTraining/OzoneTraining_4/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int t = int.Parse(Console.ReadLine()); // 4; 
+
+//var strings = new List<string[]>
+//{
+//    new[] { ">= 30" },
+//    new[] { ">= 18", "<= 23", ">= 20", "<= 27", "<= 21", ">= 28" },
+//    new[] { "<= 25", ">= 20", ">= 25" },
+//    new[] { "<= 15", ">= 30", "<= 24" }
+//};
+
+for (int i = 0; i < t; i++)
+{
+    int n = int.Parse(Console.ReadLine()); // strings[i].Length;
+
+    int result, minTemp = 15, maxTemp = 30;
+    bool impossible = false;
+
+    for (int j = 0; j < n; j++)
+    {
+        string s = Console.ReadLine(); // strings[i][j];
+
+        var (op, temp) = (s[0], int.Parse(s[2..]));
+
+        if (!impossible)
+        {
+            if (op == '>') minTemp = Math.Max(minTemp, temp);
+            else if (op == '<') maxTemp = Math.Min(maxTemp, temp);
+
+            if (minTemp > maxTemp)
+            {
+                impossible = true;
+                result = -1;
+            }
+            else
+            {
+                result = minTemp;
+            }
+        }
+        else
+        {
+            result = -1;
+        }
+        Console.WriteLine(result == -1 ? "-1" : result.ToString());
+    }
+
+    Console.WriteLine();
+}
