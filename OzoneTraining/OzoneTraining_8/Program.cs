d1bcb22c9584725bfb928ee5c13d4d2cae431c21
DotNotFact <140886710+DotNotFact@users.noreply.github.com>
2024-02-14 20:36:47 +0300

    Пробное решение задачи #8 "3-Покер"
    Не решено. Почти конечный результат

--- OzoneTraining/OzoneTraining_8/Program.cs
+++ OzoneTraining/OzoneTraining_8/Program.cs
@@ -11,129 +11,136 @@
 
 // 0
 
-int t = 4; // int.Parse(Console.ReadLine());
-
-var strings = new List<string[]>
+internal class Poker
 {
-    new[] { "TS TC", "AD AH" },
-    new[] { "2H 3H", "9S 9C", "4D QS" },
-    new[] { "4C 7H", "4H 4D", "6S 6H" },
-    new[] { "2S 3H", "2C 2D", "3C 3D" },
-};
-
-char[] suits = {
-    'C',
-    'D',
-    'S',
-    'H'
-};
-
-var chars = new HashSet<char>();
-
-for (int i = 0; i < t; i++)
-{
-    int n = strings[i].Length; // int.Parse(Console.ReadLine());
-    var cards = new List<Card>();
-
-    for (int j = 0; j < n; j++)
+    private static void Main(string[] args)
     {
-        string[] s = strings[i][j].Split(' '); // Console.ReadLine().Split(' ');
-        cards.Add(new Card(s[0], s[1]));
-    }
+        int t = 4; // int.Parse(Console.ReadLine());
 
-    var possibleCards = GetPossibleCards(cards, cards[0]);
-    Console.WriteLine(possibleCards.Count);
+        // 2 3 4 5 6 7 8 9 T J Q K A 
+        // S C D H
 
-    foreach (Card card in possibleCards)
-        Console.WriteLine(card.ToString());
-}
-
-static List<Card> GetPossibleCards(List<Card> cards, Card myCard)
-{
-    var possibleCards = new List<Card>();
-
-    // Проверяем, можем ли мы выложить сет
-    int count = 0;
-
-    for (int i = 0; i < cards.Count; i++)
-        if (myCard.Value == cards[i].Value)
-            count++;
-
-    if (count == 3)
-        possibleCards.Add(new Card(myCard.Value, myCard.Suit));
-
-    // Проверяем, можем ли мы выложить пару
-    for (int i = 0; i < cards.Count; i++)
-        if (myCard.Value == cards[i].Value)
-            possibleCards.Add(cards[i]);
-
-    // Если ни сет, ни пара невозможны, выкладываем старшую карту
-    if (possibleCards.Count == 0)
-        possibleCards.Add(GetHighestCard(cards));
-
-    return possibleCards;
-}
-
-static Card GetHighestCard(List<Card> cards)
-{
-    int maxValue = 0;
-    // int temp = 0;
-    Card maxCard = null;
-
-    var keyValuePairs = new Dictionary<char, int>()
-{
-    { '2', 2 },
-    { '3', 3 },
-    { '4', 4 },
-    { '5', 5 },
-    { '6', 6 },
-    { '7', 7 },
-    { '8', 8 },
-    { '9', 9 },
-    { 'T', 10 },
-    { 'J', 11 },
-    { 'Q', 12 },
-    { 'K', 13 },
-    { 'A', 14 },
-};
-
-    for (int i = 0; i < cards.Count; i++)
-    {
-        char first = cards[i].Value[0];
-        if (keyValuePairs.TryGetValue(first, out int temp))
+        var strings = new List<string[]>
         {
-            temp = keyValuePairs[first];
+            new[] { "TS TC", "AD AH" },
+            new[] { "2H 3H", "9S 9C", "4D QS" },
+            new[] { "4C 7H", "4H 4D", "6S 6H" },
+            new[] { "2S 3H", "2C 2D", "3C 3D" },
+        };
 
-            if (temp > maxValue)
+        for (int i = 0; i < t; i++)
+        {
+            int n = strings[i].Length; // int.Parse(Console.ReadLine());
+
+            var pokerPlayers = new List<PokerPlayer>();
+
+            for (int j = 0; j < n; j++)
             {
-                maxValue = temp;
-                maxCard = cards[i];
+                string[] s = strings[i][j].Split(' '); // Console.ReadLine().Split(' ');
+
+                for (int v = 0; v < s.Length; v++)
+                    pokerPlayers.Add(new PokerPlayer(new Card(s[v][0], s[v][1])));
             }
-        }
-        else if (temp > maxValue)
-        {
-            maxValue = temp;
-            maxCard = cards[i];
+
+            var possibleCards = GetPossibleCards(pokerPlayers);
+            Console.WriteLine(possibleCards.Count);
+
+            foreach (Card card in possibleCards)
+                Console.WriteLine(card.ToString());
         }
 
+        static List<Card> GetPossibleCards(List<PokerPlayer> cards)
+        {
+            var possibleCards = new List<Card>(); 
+
+            // Проверяем, можем ли мы выложить сет
+            int count = 0;
+
+            for (int i = 0; i < cards.Count; i++)
+                if (cards[0].Cards[0].Value == cards[i].Cards[i].Value)
+                    count++;
+
+            if (count == 3)
+                possibleCards.Add(new Card(cards[0].Cards[0].Value, cards[0].Cards[0].Suit));
+
+            // Проверяем, можем ли мы выложить пару
+            for (int i = 0; i < cards.Count; i++)
+                if (cards[0].Cards[0].Value == cards[i].Cards[i].Value)
+                    possibleCards.Add(cards[i].Cards[i]);
+
+            // Если ни сет, ни пара невозможны, выкладываем старшую карту
+            if (possibleCards.Count == 0)
+                possibleCards.Add(GetHighestCard(cards));
+
+            return possibleCards;
+        }
+
+        static Card GetHighestCard(List<Card> cards)
+        {
+            int maxValue = 0;
+            // int temp = 0;
+            Card maxCard = null;
+            var keyValuePairs = new Dictionary<char, int>()
+            {
+                { '2', 2 },
+                { '3', 3 },
+                { '4', 4 },
+                { '5', 5 },
+                { '6', 6 },
+                { '7', 7 },
+                { '8', 8 },
+                { '9', 9 },
+                { 'T', 10 },
+                { 'J', 11 },
+                { 'Q', 12 },
+                { 'K', 13 },
+                { 'A', 14 },
+            };
+
+            for (int i = 0; i < cards.Count; i++)
+            {
+                char first = cards[i].Value;
+
+                if (keyValuePairs.TryGetValue(first, out int temp))
+                {
+                    temp = keyValuePairs[first];
+
+                    if (temp > maxValue)
+                    {
+                        maxValue = temp;
+                        maxCard = cards[i];
+                    }
+                }
+                else if (temp > maxValue)
+                {
+                    maxValue = temp;
+                    maxCard = cards[i];
+                } 
+            }
+            return maxCard;
+        }
     }
-    return maxCard;
 }
 
-class Card
-{ 
-    public string Value;
-    public string Suit;
+internal class PokerPlayer
+{
+    public List<Card> Cards { get; set; } = new();
 
-    public Card(string value, string suit)
+    public PokerPlayer(Card card)
+    {
+        Cards.Add(card);
+    }
+}
+
+internal class Card
+{
+    public char Value;
+    public char Suit;
+
+    public Card(char value, char suit)
     {
         Value = value;
         Suit = suit;
-    }
-
-    public void Reverse()
-    {
-
     }
 
     public override string ToString()
